2db1603bff43aae52996782f20bff2838d6885bd
Group 21 <group21@example.com>
2026-04-12 18:31:34 +0530

    Update calculate_metrics and add print_table

--- Group21_Project2_2/metrics.c
+++ Group21_Project2_2/metrics.c
@@ -1,9 +1,21 @@
+//#include <stdio.h>
 #include "scheduler.h"
-#include <stdio.h>
 
 void calculate_metrics(Process p[], int n) {
     for (int i = 0; i < n; i++) {
         p[i].turnaround = p[i].completion - p[i].arrival;
-        p[i].waiting = p[i].turnaround - p[i].burst;
+        p[i].waiting    = p[i].turnaround - p[i].burst;
     }
 }
+
+void print_table(Process p[], int n) {
+    printf("\n%-5s %-8s %-8s %-12s %-12s %-10s %-10s\n",
+           "PID", "Arrival", "Burst", "Completion", "Turnaround", "Waiting", "Response");
+    for (int i = 0; i < n; i++) {
+        printf("%-5d %-8d %-8d %-12d %-12d %-10d %-10d\n",
+               p[i].pid, p[i].arrival, p[i].burst,
+               p[i].completion, p[i].turnaround,
+               p[i].waiting, p[i].response);
+    }
+}
+//
